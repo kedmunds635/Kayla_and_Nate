b0c3d52289f355473ab47474e4f810c1691bffa1
NathanielSmall <70652066+NathanielSmall@users.noreply.github.com>
2020-10-13 10:21:05 -0500

    created files & interfafce

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+public interface Car {
+
+    public void move();
+
+    public void checkForCollision();
+
+    public double getMass();
+
+    public double getDx();
+
+    public double getDy();
+
+    public double getX();
+
+    public double getY();
+}
